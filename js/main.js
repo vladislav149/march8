c58d07dd3c1b3eafa0e5ea913fe88f9a2dd56aac
vladislav149 <vladislav149@mail.ru>
2024-03-07 14:35:53 +0300

    fixed load

--- js/main.js
+++ js/main.js
@@ -4,13 +4,19 @@ const textArr = [...text]
 let index = 0
 const max = textArr.length
 
-const textEl = document.querySelector('.text')
+const img = new Image()
+img.src = 'content/q.gif'
+img.classList.add('img')
+img.onload = function () {
+  document.body.insertAdjacentElement('afterbegin', img)
+  document.querySelector('.wrapper').innerHTML = `
+  <div class="container">
+        <p class="text"></p>
+        <span class="author">SmartDocs Team</span>
+      </div>
+  `
+  const textEl = document.querySelector('.text')
 
-const a = new Image()
-a.src = 'content/q.gif'
-a.classList.add('img')
-a.onload = function () {
-  document.body.insertAdjacentElement('afterbegin', a)
   const interval = setInterval(() => {
     if (index === max) {
       clearInterval(interval)
