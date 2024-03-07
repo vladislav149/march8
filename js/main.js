4eaa42f67b0c7a99193fe68e174aa87f09ea3eee
vladislav149 <vladislav149@mail.ru>
2024-03-07 17:48:46 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,28 +1,54 @@
-const text =
-  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem dolorem ipsum hic minima ipsa culpa. Cupiditate ducimus dignissimos quaerat iure et, assumenda, a incidunt ab obcaecati ipsa atque! Omnis?'
-const textArr = [...text]
-let index = 0
-const max = textArr.length
+const TITLE = 'Дорогие Оля, Полина и София!'
+const TEXT1 =
+  'Поздравляем вас с весенним праздником 8 марта! Пусть этот день принесет вам море радости, улыбок и теплоты. Пусть каждый цветок, каждый лучик солнца напоминают вам о вашей неповторимой красоте и уникальности.'
+const TEXT2 =
+  'Пусть ваши сердца наполняются любовью и теплом, а каждый день приносит новые возможности и счастливые моменты. Пусть ваши мечты сбудутся, а каждый шаг приведет к успеху.'
+const TEXT3 =
+  'С праздником весны, с 8 марта! Пусть ваш день будет ярким, нежным и наполненным радостью.'
+const TEXT4 = 'SmartDocs Team'
 
 const img = new Image()
 img.src = 'content/petals.gif'
 img.classList.add('img')
-img.onload = function () {
+img.onload = async function () {
   document.body.insertAdjacentElement('afterbegin', img)
   document.querySelector('.wrapper').innerHTML = `
     <div class="container">
-      <p class="text"></p>
-      <span class="author">SmartDocs Team</span>
+      <div>
+        <h1 class="title"></h1>
+        <p class="text text1"></p>
+        <p class="text text2"></p>
+        <p class="text text3"></p>
+      </div>
+      <p class="author"></p>
     </div>
   `
-  const textEl = document.querySelector('.text')
+  const titleEl = document.querySelector('h1')
+  const textEl1 = document.querySelector('.text1')
+  const textEl2 = document.querySelector('.text2')
+  const textEl3 = document.querySelector('.text3')
+  const textEl4 = document.querySelector('.author')
+  await tapText(TITLE, titleEl)
+  await tapText(TEXT1, textEl1)
+  await tapText(TEXT2, textEl2)
+  await tapText(TEXT3, textEl3)
+  tapText(TEXT4, textEl4)
+}
 
-  const interval = setInterval(() => {
-    if (index === max) {
-      clearInterval(interval)
-      return
-    }
-    textEl.textContent += textArr[index]
-    index++
-  }, 100)
+function tapText(text, htmlElement) {
+  const INTERVAL = 70
+  let index = 0
+  const textArr = [...text]
+  const max = textArr.length
+
+  return new Promise(resolve => {
+    const intervalId = setInterval(() => {
+      if (index === max) {
+        clearInterval(intervalId)
+        return resolve()
+      }
+      htmlElement.textContent += textArr[index]
+      index++
+    }, INTERVAL)
+  })
 }
