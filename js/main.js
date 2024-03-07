77c7990374b8d9e62bb423da3e8897d7b7a64816
vladislav149 <vladislav149@mail.ru>
2024-03-07 13:21:46 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const text =
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae voluptatem dolorem ipsum hic minima ipsa culpa. Cupiditate ducimus dignissimos quaerat iure et, assumenda, a incidunt ab obcaecati ipsa atque! Omnis?'
+const textArr = [...text]
+let index = 0
+const max = textArr.length
+
+const textEl = document.querySelector('.text')
+const interval = setInterval(() => {
+  if (index === max) {
+    textEl.textContent = ''
+    index = 0
+    return
+  }
+  textEl.textContent += textArr[index]
+  index++
+}, 100)
